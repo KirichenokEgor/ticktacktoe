6146201d580ecbf67aad6e34e0abcbe6ea2ee0f2
KirichenokEgor <kirieg.official@gmail.com>
2020-04-01 11:20:51 +0300

    Created SpringBoot application with help of Spring Initializr

---
+++ src/test/java/com/kirychonak/study/cn/ticktacktoe/TicktacktoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirychonak.study.cn.ticktacktoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicktacktoeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
